f1a35ebded4fae5d9f84575dffea38cfe8f9e68f
Anand06s <qwertyanand@protonmail.com>
2025-11-10 10:18:20 +0530

    Delete decisionimporter directory

--- decisionimporter/ui/services.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- SPDX-FileCopyrightText: © Fossology contributors
-
- SPDX-License-Identifier: GPL-2.0-only
-*/
-$loader = $GLOBALS['container']->get('twig.loader');
-$loader->addPath(dirname(__FILE__).'/template');
